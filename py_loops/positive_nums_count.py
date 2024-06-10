e30d4a4b158b92291b1ae5cd0597a0c55613b462
Shankar Aswal <shankaraswal@gmail.com>
2024-06-10 21:32:27 +0100

    sol: positive numers count in an arr

---
+++ py_loops/positive_nums_count.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, -3, 4, -5, -6, 7, -8, 9, -10]
+
+positiv_number = 0 
+
+for num in numbers:
+    if num > 0:
+        positiv_number += 1
+print("final count of positive number ",  positiv_number)
