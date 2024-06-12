69035e8fe6bd8ac8bd7eafd7ce0ef9c6fb89ca0a
Shankar Aswal <shankaraswal@gmail.com>
2024-06-12 11:01:39 +0100

    **kwargs as key val pair

---
+++ py_funcitons/keywords_args_fn.py
@@ -0,0 +1,12 @@
+
+def name(**kwargs):
+    for k, v in kwargs.items():
+        print (k, v)
+        
+    
+
+name(name="aaa", age=15)
+name(name="bbb", surname="aswal", age=25)
+name(age=35)
+    
+    
