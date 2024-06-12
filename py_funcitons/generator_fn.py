9900878a8662b80f6bd6a54ebb2bf51a2cee0999
Shankar Aswal <shankaraswal@gmail.com>
2024-06-12 11:21:09 +0100

    generator funciton using skip value, start and end range

--- py_funcitons/generator_fn.py
+++ py_funcitons/generator_fn.py
@@ -1,1 +1,8 @@
-# init commit
+
+def num_skipper(start, end, skip_count):
+    for i in range(start, end+1, skip_count):
+        yield i
+        
+        
+for num in num_skipper(10, 50, 3):
+    print(num)
